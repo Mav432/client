a2afd70d1b7c11e04492dafe610b61c590931942
MavAnt <adwr789@gmail.com>
2025-01-30 03:52:22 -0600

    commit api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,6 @@ import Navbar from '../components/Navbar';
 function HomePage() {
   return (
     <div>
-      <Navbar />
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
         <h1 className="text-4xl font-bold text-white">Bienvenido a AppName</h1>
       </div>
